c0213d46c52a2d0d72c977b854dc0451ffffb1ac
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-08-12 15:23:17 +0300

    Add price inputs and range sliders.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, Topbar } from "@/components/shared";
+import { Container, Filters, Title, Topbar } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -6,9 +6,20 @@ export default function Home() {
       <Container className="mt-10">
         <Title text="All pizzas" size="lg" className="font-extrabold" />
       </Container>
+
       <Topbar />
 
-      <div style={{ height: "200vh" }}></div>
+      <Container className="pb-14 mt-10">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Items List</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
