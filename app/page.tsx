4a4b39be2bbd2dd005f4a88122d41221f529e970
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-09-09 13:06:23 +0300

    Make categories and products render dynamically.

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,29 @@ import {
   Title,
   Topbar,
 } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Container className="mt-10">
         <Title text="All pizzas" size="lg" className="font-extrabold" />
       </Container>
 
-      <Topbar />
+      <Topbar
+        categories={categories.filter((item) => item.products.length > 0)}
+      />
 
       <Container className="pb-14 mt-10">
         <div className="flex gap-[80px]">
@@ -23,118 +37,17 @@ export default function Home() {
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
-                title="Pizzas"
-                items={[
-                  {
-                    id: 1,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                  {
-                    id: 2,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                  {
-                    id: 3,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                  {
-                    id: 4,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                  {
-                    id: 5,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EE7D612FC7B7FCA5BE822752BEE1E5.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                ]}
-                categoryId={1}
-              />
-
-              <ProductsGroupList
-                title="Breakfast"
-                items={[
-                  {
-                    id: 10,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EF112C05B1B9C193648449783C1A82.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                  {
-                    id: 11,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EF112C05B1B9C193648449783C1A82.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                  {
-                    id: 12,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EF112C05B1B9C193648449783C1A82.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                  {
-                    id: 13,
-                    name: "Margarita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:584x584/11EF112C05B1B9C193648449783C1A82.avif",
-                    items: [
-                      {
-                        price: 124,
-                      },
-                    ],
-                  },
-                ]}
-                categoryId={2}
-              />
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  )
+              )}
             </div>
           </div>
         </div>
