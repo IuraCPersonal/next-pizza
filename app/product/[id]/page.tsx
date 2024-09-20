58718471025076d27e7ec2fb0cfb04908cc7dcc2
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-09-20 15:53:43 +0300

    Setup product info page.

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,64 @@
-export default function ProductPage({
+import { notFound } from "next/navigation";
+import { prisma } from "@/prisma/prisma-client";
+
+import {
+  Container,
+  GroupVariants,
+  ProductImage,
+  Title,
+} from "@/components/shared";
+
+export default async function ProductPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <div>Product Page</div>;
+  const product = await prisma.product.findFirst({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={30} />
+
+        <div className="w-[490px] bg-[#FCFCFC] p-7">
+          <Title
+            text={product.name}
+            size="md"
+            className="font-extrabold mb-1"
+          />
+
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut facilis
+          </p>
+
+          <GroupVariants
+            value="2"
+            items={[
+              {
+                name: "Small",
+                value: "1",
+              },
+              {
+                name: "Medium",
+                value: "2",
+              },
+              {
+                name: "Large",
+                value: "3",
+                disabled: true,
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -9,3 +9,5 @@ export { FilterCheckbox } from "./filter-checkbox";
 export { RangeSlider } from "./range-slider";
 export { ProductsGroupList } from "./products-group-list";
 export { SearchInput } from "./search-input";
+export { ProductImage } from "./product-image";
+export { GroupVariants } from "./group-variants";
