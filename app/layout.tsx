d21123f5c8746ad6ad2b016fde16c2f9948a7bcb
IuraCPersonal <iurie.cius.personal@gmail.com>
2024-12-31 13:08:48 +0200

    chore: small changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+
 import "./globals.css";
-import { Header } from "@/components/shared/header";
 
 const nunito = Nunito({
   subsets: ["cyrillic"],
@@ -9,23 +8,18 @@ const nunito = Nunito({
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-export const metadata: Metadata = {
-  title: "Next Pizza | Main",
-};
-
-export default function RootLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        <main className="min-h-screen">
-          <Header />
-          {children}
-        </main>
-      </body>
+      <head>
+        <link data-rh="true" rel="icon" href="/logo.png" />
+      </head>
+
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
